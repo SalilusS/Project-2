f79139065e60cf64d52089ae6a3fd912e8e3de5c
SalilusS <44485874+SalilusS@users.noreply.github.com>
2018-11-19 21:25:43 +0700

    Update Main.py
    +Project

---
+++ Main.py
@@ -0,0 +1,26 @@
+# Project-2
+
+import random
+number = random.randint(1000, 9999)
+m_number = [number//1000, number%1000 - number%100, number%100 - number%10, number%10]
+def search():
+    true = 0
+    try:
+        player = int(input())
+    except ValueError:
+        player = 0
+    if 1 <= (player//1000) <= 9:
+        m_player = [player//1000, player%1000 - player%100, player%100 - player%10, player%10]
+        for step in range(4):
+            if m_player[step] == m_number[step]:
+                true += 1
+        if true == 4:
+            print("Браво! Загаданное число:", player)
+        else:
+            print(true, "верно!")
+            search()
+    else:
+        print("Введите четырёхзначное число!")
+        search()
+print("Введите четырёхзначное число:")
+search()
