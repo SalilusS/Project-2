2128a7de4e0affac994420e6503ee7a747d621dc
SalilusS <44485874+SalilusS@users.noreply.github.com>
2018-11-20 00:03:05 +0700

    Update Main.py
    +Система уровней

--- Main.py
+++ Main.py
@@ -1,9 +1,24 @@
-# Project-2
-
 import random
+
 number = random.randint(1000, 9999)
 m_number = [number//1000, number%1000 - number%100, number%100 - number%10, number%10]
-def search():
+
+def level():
+    print("Выберите уровень сложности:")
+    print("1. Лёгкий")
+    print("2. Средний")
+    print("3. Сложный")
+    try:
+        level = int(input())
+    except ValueError:
+        print("БУДЕШЬ ИГРАТЬ НА СЛОЖНОМ!")
+        return 2
+    if 1 <= level <= 3:
+        return 11 - level**2
+    else:
+        print("БУДЕШЬ ИГРАТЬ НА СЛОЖНОМ!")
+        return 2
+def search(limit):
     true = "-"
     try:
         player = int(input())
@@ -15,12 +30,18 @@ def search():
             if m_player[step] == m_number[step]:
                 true = true + str(step+1)
         if player == number:
-            print("Браво! Загаданное число:", player)
+            print("Молодчина! Загаданное мной число действительно:", player)
         else:
             print(true)
-            search()
+            if limit != 0:
+                limit -= 1
+                search(limit)
+            else:
+                print("Ай да молодец! Снова не угадал!")
     else:
-        print("Введите четырёхзначное число!")
-        search()
+        print("Введите четырёхзначное число >.<")
+        search(limit)
+
+limit = level()
 print("Введите четырёхзначное число:")
-search()
+search(limit)
