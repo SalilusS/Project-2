cce76fe473ac1e672f0ae962152ffeb5d6436cda
SalilusS <44485874+SalilusS@users.noreply.github.com>
2018-11-20 18:49:18 +0700

    Update Main.py
    Local + Последние штрихи

--- Main.py
+++ Main.py
@@ -1,22 +1,23 @@
+import Local
 import random
 
 number = random.randint(1000, 9999)
 m_number = [number//1000, number%1000 - number%100, number%100 - number%10, number%10]
 
 def level():
-    print("Выберите уровень сложности:")
-    print("1. Лёгкий")
-    print("2. Средний")
-    print("3. Сложный")
+    print(Local.RU_CHOOSE_A_DIFFICULTY_LEVEL)
+    print(Local.RU_EASY)
+    print(Local.RU_MEDIUM)
+    print(Local.RU_HARD)
     try:
         level = int(input())
     except ValueError:
-        print("БУДЕШЬ ИГРАТЬ НА СЛОЖНОМ!")
+        print(Local.RU_ANGRY_ANSWER)
         return 2
     if 1 <= level <= 3:
         return 11 - level**2
     else:
-        print("БУДЕШЬ ИГРАТЬ НА СЛОЖНОМ!")
+        print(Local.RU_ANGRY_ANSWER)
         return 2
 def search(limit):
     true = "-"
@@ -28,20 +29,20 @@ def search(limit):
         m_player = [player//1000, player%1000 - player%100, player%100 - player%10, player%10]
         for step in range(4):
             if m_player[step] == m_number[step]:
-                true = true + str(step+1)
+                true = true[:-1] + str(step+1) + "."
         if player == number:
-            print("Молодчина! Загаданное мной число действительно:", player)
+            print(Local.RU_CONGRATULATIONS, player)
         else:
             print(true)
             if limit != 0:
                 limit -= 1
                 search(limit)
             else:
-                print("Ай да молодец! Снова не угадал!")
+                print(Local.RU_MOCKERY)
     else:
-        print("Введите четырёхзначное число >.<")
+        print(Local.RU_ANGRY_INPUT_A_FOUR_DIGIT_NUMBER)
         search(limit)
 
 limit = level()
-print("Введите четырёхзначное число:")
+print(Local.RU_INPUT_A_FOUR_DIGIT_NUMBER)
 search(limit)
